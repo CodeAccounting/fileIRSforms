80c3fb7c07e9cf6877a051376c04f062b861d96f
Dejan <dejansabados@yahoo.com>
2017-09-07 23:02:48 +0200

    dashboard changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!, only: [:home]
   def home
-    @submissions = Field.find_by_sql("SELECT DISTINCT ON (unique_id) unique_id, form_id , updated_at, labelcolor FROM Fields WHERE user_id= #{current_user.id}")
+    if (params[:label].present?)
+        @submissions = Field.find_by_sql("SELECT DISTINCT ON (unique_id) unique_id, form_id , updated_at, labelcolor FROM Fields WHERE user_id= #{current_user.id} AND label='#{params[:label]}'")
+        @selected_label = params[:label];
+    else
+        @submissions = Field.find_by_sql("SELECT DISTINCT ON (unique_id) unique_id, form_id , updated_at, labelcolor FROM Fields WHERE user_id= #{current_user.id}")
+        @selected_label = ''
+    end
     @labels = Field.where(user_id: current_user.id).where.not(label: nil,label: "" ).pluck(:label,:labelcolor).uniq
   end
 end
--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -2,23 +2,3 @@ module AdminHelper
     def addT(data)
   end
 end
-=begin
-module UsersHelper
-  def full_name(user)
-    user.first_name + user.last_name
-  end
-end
-
-class UsersController < ApplicationController
-
-  def update
-    @user = User.find params[:id]
-    if @user.update_attributes(user_params)
-      notice = "#{helpers.full_name(@user) is successfully updated}"
-      redirect_to user_path(@user), notice: notice
-    else
-      render :edit
-    end
-  end
-end
-=end
