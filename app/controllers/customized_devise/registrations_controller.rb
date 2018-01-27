550bee852342fa5c41b87e9ef46adec74b84bc9a
Dejan <dejansabados@yahoo.com>
2018-01-27 01:13:14 +0100

    backup 27jan2018

--- app/controllers/customized_devise/registrations_controller.rb
+++ app/controllers/customized_devise/registrations_controller.rb
@@ -1,9 +1,12 @@
 class CustomizedDevise::RegistrationsController < Devise::RegistrationsController
   protected
-    def after_sign_up_path_for(resource_or_scope)  
-    @email_value=Referring.where(code: '7878878').first #params[:referee_code]
+    def after_sign_up_path_for(resource_or_scope)
+    @ref = Referring.where(code: params[:referee_code]).first
+    @email_value= @ref.email
+    #render :text => 'works' and return
     refferedUser=User.where(email: @email_value).first 
     refferedUser.increment!(:bonus)
+    @ref.destroy
     super(resource)
   end
 end
